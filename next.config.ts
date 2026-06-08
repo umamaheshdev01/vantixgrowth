7060311114363064beb43dc8a38df27bd354f1c1
Uma Mahesh <umamahesh@Umas-MacBook-Air.local>
2026-06-08 22:13:11 +0530

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pg', '@prisma/adapter-pg'],
+  serverExternalPackages: ['pg', '@prisma/adapter-pg', '@prisma/client'],
 }
 
 export default nextConfig
